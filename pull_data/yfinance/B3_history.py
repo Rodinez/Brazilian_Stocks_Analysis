8341a4c4e2514e989b5dbe5af3dda190135d2b82
Rodinez <mngtandrade@gmail.com>
2026-08-22 00:20:24 -0300

    historical from stocks

--- pull_data/yfinance/B3_history.py
+++ pull_data/yfinance/B3_history.py
@@ -2,7 +2,9 @@ import os
 import json
 import requests
 from datetime import datetime
+from zoneinfo import ZoneInfo
 import yfinance as yf
+import pandas as pd
 from dotenv import load_dotenv
 
 load_dotenv("../.env")
@@ -51,12 +53,13 @@ def get_stocks():
 
         page += 1
 
+    stocks = [stock for stock in stocks if not stock.endswith("F")]
     return sorted(set(stocks))
 
 
 def should_update():
     if not os.path.exists(METADATA_PATH):
-        return True
+        return True, "2010-01-01"
 
     with open(METADATA_PATH, encoding="utf-8") as f:
         metadata = json.load(f)
@@ -64,8 +67,14 @@ def should_update():
     last_update = datetime.fromisoformat(
         metadata["last_update"]
     )
-
-    return last_update.date() < datetime.now().date()
+    
+    tz_sp = ZoneInfo("America/Sao_Paulo")
+    time_sp = int(datetime.now(tz_sp).strftime("%H"))
+    
+    update = True if (last_update.date() < datetime.now().date()) or (last_update.date() == datetime.now().date() and (time_sp >= 10 and time_sp <= 17)) else False
+    start_date = str(last_update.strftime("%Y-%m-%d"))
+    
+    return update, start_date
 
 
 def update_metadata():
@@ -78,7 +87,7 @@ def update_metadata():
             indent=4
         )
 
-def download_yfinance(stocks):
+def download_yfinance(stocks, start_date):
     os.makedirs(YFINANCE_PATH, exist_ok=True)
 
     failed = []
@@ -92,20 +101,30 @@ def download_yfinance(stocks):
             try:
                 data = yf.download(
                     ticker,
-                    start="2010-01-01",
+                    start=start_date,
                     auto_adjust=False,
                     progress=False
                 )
 
                 if data.empty:
                     raise ValueError("Sem dados")
+                
+                if len(data) < 100:
+                    print(f"{ticker}: apenas {len(data)} registros")    
 
-                path = os.path.join(
-                    YFINANCE_PATH,
-                    f"{stock}.csv"
-                )
+                data = data.reset_index()
+                data.columns = ["Date", "Adj Close", "Close", "High", "Low", "Open", "Volume"]
+                data["Date"] = data["Date"].dt.date
+                
+                path = os.path.join(YFINANCE_PATH, f"{stock}.csv")
 
-                data.to_csv(path)
+                if os.path.exists(path):
+                    historical = pd.read_csv(path)
+                    historical = historical.iloc[:-1]
+
+                    data = pd.concat([historical, data], ignore_index=True)
+
+                data.to_csv(path, index=False)
 
                 print(f"Salvo: {path}")
                 break
@@ -122,20 +141,17 @@ def download_yfinance(stocks):
         print(ticker)
 
 def update_stocks():
-    if not should_update():
-        print("Lista de ações já consultada hoje.")
+    update, start_date = should_update()
+    
+    if not update:
         return None
 
     stocks = get_stocks()
-
-    download_yfinance(stocks)
-
+    download_yfinance(stocks, start_date)
     update_metadata()
 
     print(f"{len(stocks)} ações encontradas.")
-
     return stocks
-
 
 if __name__ == "__main__":
     stocks = update_stocks()
